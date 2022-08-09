f753559484a8b72baae74e628b15a40cbbf36a6d
Roy Rios <roymillerrz@gmail.com>
2022-08-08 23:35:14 -0500

    new proyecto .. otra vez

---
+++ Proyecto-austral-sac/src/test/java/com/g5/app/ProyectoAustralSacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.g5.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoAustralSacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
