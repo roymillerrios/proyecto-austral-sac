06c0c7dc26c53938ed48996948aea268219f1263
Roy Rios <roymillerrz@gmail.com>
2022-08-29 21:04:00 -0500

    f

--- Proyecto-austral-sac/src/main/resources/import.sql
+++
@@ -1,7 +0,0 @@
-
-INSERT INTO trabajadores (password, email) VALUES('$2a$10$Abw2fx8bb97LSM4SOB1D4.qU6PQWJy9jCOubDpcy6fLBkI0sNtXCi', 'admin@australsac.com');
-
-INSERT INTO roles (descripcion) VALUES('ADMIN');
-
-INSERT INTO trabajador_rol (trabajador_id,rol_id) VALUES(1,1);
-
